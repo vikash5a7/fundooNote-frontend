5a6f73183a549bd99bbe58fd7ae7fcceb79e68e1
vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-03-09 19:23:01 +0530

    [vikash]added. registration connected with backend

--- src/app/Services/token.service.ts
+++ src/app/Services/token.service.ts
@@ -9,9 +9,32 @@ export class TokenService {
   public handle(token) {
     console.log('token is going to be set');
     this.set(token);
+    console.log('decoded form is ' + JSON.stringify(this.payLoad(token)));
   }
+  // setting token in local
   set(token) {
-    console.log('token is going to be set');
     localStorage.setItem('token', token);
+  }
+  // getting token from the local storage
+  get() {
+   return localStorage.getItem('token');
+  }
+  // Removing item from the local storage
+  remove() {
+    localStorage.removeItem('token');
+  }
+  isValid() {
+    const token = this.get();
+    if (this.get()) {
+      const payload = this.payLoad(token);
+    }
+  }
+  payLoad(token) {
+    const payload = token.split('.')[1];
+    console.log('payload is : ' + payload);
+    return this.decode(payload);
+  }
+  decode(payload) {
+    return JSON.parse(atob(payload));
   }
 }
